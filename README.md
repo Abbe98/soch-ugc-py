faccdff4e351d60aa07cf35df26cdbc3ffc6c1ce
Albin <albin.larsson@raa.se>
2018-10-04 16:14:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# SOCH UGC
+
+SOCH UGC is a Python library for accessing and writing data to the [User Generated Content API](https://www.raa.se/hitta-information/k-samsok/anvandargenererat-innehall-ugc-hubben/) that is a part of the Swedish Open Cultural Heritage (SOCH/K-Samsök) API.
+
+## Install SOCH UGC
+
+```bash
+pip install sochugc
+```
+
+## Usage Examples
+
+```python
+from sochugc import UGC
+
+# the endpoint defaults to https://ugc.kulturarvsdata.se/
+ugc = UGC('<API-KEY>', endpoint='http://lx-ra-ugchubtest:8080/')
+
+# get the total number of user generated items
+ugc.get_total_items_count()
+
+# return an user generated content item by its id
+ugc.get_item(679)
+
+# Searching all items or items tied to a specific URI
+# to search all items and not only ones connected to an specific URI omit the uri parameter
+ugc.search_items(uri='http://kulturarvsdata.se/raa/bbr/21400000440954', offset=0, limit=100)
+
+#
+# Writing data is not available for everyone
+#
+
+# deleting an item using its id
+ugc.delete_item(679)
+
+# creating a new relation, see below for possible relations
+ugc.create_item_relation('http://kulturarvsdata.se/raa/kmb/16001000004075', 'isPartOf', 'http://kulturarvsdata.se/pm/photo/POST036605', 'Albin Larsson')
+```
+
+## Supported Item Relations
+
+ - sameAs
+ - isDescribedBy
+ - visualizes
+ - hasPart
+ - isPartOf
+ - isVisualizedBy
+ - isContainedIn
+ - author
+ - authorOf
+ - hasBeenUsedIn
+ - isRelatedTo
+ - architectOf
+ - architect
+ - user
+ - userOf
+ - child
+ - mother
+ - father
+ - photographerOf
+ - photographer
+ - isMentionedBy
+ - mentions
---
+++ sochugc/__init__.py
@@ -0,0 +1,124 @@
+import requests
+from ksamsok import KSamsok
+
+class UGC:
+    def __init__(self, key, endpoint='http://ugc.kulturarvsdata.se/'):
+        self.endpoint = endpoint + 'UGC-hub/'
+        self.key = key
+
+        self.headers = {
+            'User-Agent': 'SOCH-UGC.py'
+        }
+
+        self.soch = KSamsok()
+
+        self.relation_types = list([
+            'sameAs',
+            'isDescribedBy',
+            'visualizes',
+            'hasPart',
+            'isPartOf',
+            'isVisualizedBy',
+            'isContainedIn',
+            'author',
+            'authorOf',
+            'hasBeenUsedIn',
+            'isRelatedTo',
+            'architectOf',
+            'architect',
+            'user',
+            'userOf',
+            'child',
+            'mother',
+            'father',
+            'photographerOf',
+            'photographer',
+            'isMentionedBy',
+            'mentions'
+        ])
+
+        # so that the user get endpoint/key errors early
+        self.get_total_items_count()
+
+    def get_total_items_count(self):
+        url = '{}api?x-api={}&method=retrieve&scope=count&objectUri=all&format=json'.format(self.endpoint, self.key)
+        data = self.make_get_request(url)
+
+        return data['response']['relations']['numberOfRelations']
+
+    def get_item(self, item_id):
+        url = '{}api?x-api={}&method=retrieve&objectUri=all&contentId={}&scope=single&format=json'.format(self.endpoint, self.key, item_id)
+        data = self.make_get_request(url)
+
+        if data['response']['relations'][0]['id'] is 0:
+            return False
+
+        return data['response']['relations'][0]
+
+    def search_items(self, uri='all', offset=0, limit=50):
+        url = '{}api?x-api={}&method=retrieve&scope=all&objectUri={}&selectFrom={}&maxCount={}&format=json'.format(self.endpoint, self.key, uri, offset, limit)
+        data = self.make_get_request(url)
+
+        return data['response']['relations']
+
+    def delete_item(self, item_id):
+        url = '{}api?x-api={}&method=delete&objectId={}&format=json'.format(self.endpoint, self.key, item_id)
+        data = self.make_get_request(url)
+
+        if data['response']['result'] == 'SUCCESS':
+            return True
+        return False
+
+    def create_item_relation(self, kulturarvsdata_uri, relation, target, user):
+        if not self.soch.formatUri(kulturarvsdata_uri, 'rawurl'):
+            raise ValueError('{} is not an valid Kulturarvsdata URI.'.format(kulturarvsdata_uri))
+
+        if relation not in self.relation_types:
+            raise ValueError('{} is not a valid relation type.'.format(relation))
+
+        if not self.valid_relation_target(target):
+            raise ValueError('{} is not a valid target.'.format(target))
+
+        url = '{}api?x-api={}&method=insert&scope=relationAll&objectUri={}&user={}&relationType={}&relatedTo={}&format=json'.format(self.endpoint, self.key, kulturarvsdata_uri, user, relation, target)
+        print(url)
+        data = self.make_get_request(url)
+
+        if data['response']['result'] == 'SUCCESS':
+            return True
+        return False
+
+    def valid_relation_target(self, target):
+        if target.startswith('http://kulturarvsdata.se/'):
+            if not self.soch.formatUri(target, 'rawurl'):
+                return False
+            return True
+
+        if target.startswith('https://commons.wikimedia.org/wiki/File:'):
+            return True
+
+        if target.startswith('https://commons.wikimedia.org/wiki/Category:'):
+            return True
+
+        if target.startswith('http://www.wikidata.org/entity/Q'):
+            return True
+
+        if target.startswith('https://sv.wikipedia.org/wiki/'):
+            return True
+
+        if target.startswith('https://en.wikipedia.org/wiki/'):
+            return True
+
+        return False
+
+    def make_get_request(self, url):
+        r = requests.get(url, headers=self.headers)
+
+        # note UGC always returns 200 codes for now.
+        if r.status_code is not 200:
+            raise ValueError('Could not access endpoint.')
+
+        data = r.json()
+        if 'error' in data['response']:
+            raise ValueError('Bad API key.')
+
+        return data
