4a2579872cb906ef4b2f1de76f465cfe7608c217
Abbe98 <albin.post@gmail.com>
2020-05-20 16:09:31 +0200

    compability with the new UGC API

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SOCH UGC
 
-SOCH UGC is a Python library for accessing and writing data to the [User Generated Content API](https://www.raa.se/hitta-information/k-samsok/anvandargenererat-innehall-ugc-hubben/) that is a part of the Swedish Open Cultural Heritage (SOCH/K-Samsök) API.
+SOCH UGC is a Python library for accessing and writing data to the [User Generated Content API](https://ugc.kulturarvsdata.se/UGC-hub/) that is a part of the Swedish Open Cultural Heritage (SOCH/K-Samsök) API.
 
 ## Install SOCH UGC
 
@@ -13,8 +13,9 @@ pip install sochugc
 ```python
 from sochugc import UGC
 
-# the endpoint defaults to http://ugc.kulturarvsdata.se/
-ugc = UGC('<API-KEY>', endpoint='http://lx-ra-ugchubtest:8080/')
+# the endpoint defaults to https://ugc.kulturarvsdata.se/
+# the key parameter is only needed for write actions
+ugc = UGC(endpoint='http://localhost:8080/', key='<API-KEY>')
 
 # get the total number of user generated items
 ugc.get_total_items_count()
@@ -23,12 +24,8 @@ ugc.get_total_items_count()
 ugc.get_item(679)
 
 # Searching all items or items tied to a specific URI
-# to search all items and not only ones connected to an specific URI omit the uri parameter
+# to search all items and not only ones connected to a specific URI omit the uri parameter
 ugc.search_items(uri='http://kulturarvsdata.se/raa/bbr/21400000440954', offset=0, limit=100)
-
-#
-# Writing data is not available for everyone
-#
 
 # deleting an item using its id
 ugc.delete_item(679)
--- sochugc/__init__.py
+++ sochugc/__init__.py
@@ -6,7 +6,7 @@ import requests
 from ksamsok import KSamsok
 
 class UGC:
-    def __init__(self, key: str, endpoint: str = 'http://ugc.kulturarvsdata.se/') -> None:
+    def __init__(self, endpoint: str = 'https://ugc.kulturarvsdata.se/', key: str = None) -> None:
         self.endpoint = endpoint + 'UGC-hub/'
         self.key = key
 
@@ -41,17 +41,14 @@ class UGC:
             'mentions'
         ])
 
-        # so that the user get endpoint/key errors early
-        self.get_total_items_count()
-
     def get_total_items_count(self) -> str:
-        url = '{}api?x-api={}&method=retrieve&scope=count&objectUri=all&format=json'.format(self.endpoint, self.key)
+        url = '{}api?method=retrieve&scope=count&objectUri=all&format=json'.format(self.endpoint)
         data = self.make_get_request(url)
 
         return data['response']['relations']['numberOfRelations']
 
     def get_item(self, item_id: Union[int, str]) -> Union[Dict, bool]:
-        url = '{}api?x-api={}&method=retrieve&objectUri=all&contentId={}&scope=single&format=json'.format(self.endpoint, self.key, item_id)
+        url = '{}api?method=retrieve&objectUri=all&contentId={}&scope=single&format=json'.format(self.endpoint, item_id)
         data = self.make_get_request(url)
 
         if data['response']['relations'][0]['id'] is 0:
@@ -60,7 +57,7 @@ class UGC:
         return data['response']['relations'][0]
 
     def search_items(self, uri: str = 'all', offset: int = 0, limit: int = 50) -> List:
-        url = '{}api?x-api={}&method=retrieve&scope=all&objectUri={}&selectFrom={}&maxCount={}&format=json'.format(self.endpoint, self.key, uri, offset, limit)
+        url = '{}api?method=retrieve&scope=all&objectUri={}&selectFrom={}&maxCount={}&format=json'.format(self.endpoint, uri, offset, limit)
         data = self.make_get_request(url)
 
         return data['response']['relations']
@@ -68,6 +65,9 @@ class UGC:
     def delete_item(self, item_id: Union[int, str]) -> bool:
         url = '{}api?x-api={}&method=delete&objectId={}&format=json'.format(self.endpoint, self.key, item_id)
         data = self.make_get_request(url)
+
+        if not self.key:
+            raise ValueError('This action requires an API key.')
 
         if data['response']['result'] == 'SUCCESS':
             return True
@@ -83,6 +83,9 @@ class UGC:
 
         if not self.valid_relation_target(target):
             raise ValueError('{} is not a valid target.'.format(target))
+
+        if not self.key:
+            raise ValueError('This action requires an API key.')
 
         url = '{}api?x-api={}&method=insert&scope=relationAll&objectUri={}&user={}&relationType={}&relatedTo={}&format=json'.format(self.endpoint, self.key, kulturarvsdata_uri, user, relation, target)
         data = self.make_get_request(url)
@@ -121,11 +124,11 @@ class UGC:
         r = requests.get(url, headers = self.headers)
 
         # note UGC always returns 200 codes for now.
-        if r.status_code is not 200:
-            raise ValueError('Could not access endpoint.')
+        if r.status_code is 401:
+            raise PermissionError('Bad API key.')
 
         data = r.json()
         if 'error' in data['response']:
-            raise ValueError('Bad API key.')
+            raise Exception('Unknown error: {}'.format(data['response']['error']))
 
         return data
